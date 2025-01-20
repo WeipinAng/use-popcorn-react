b4831b8016c43471bb4121dd4c228477b19eb371
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-21 01:44:45 +0800

    fetch data from omdb api but with infinite number of requests in network tab (due to setting state in render logic causing component to re-render itself again)

--- src/App.js
+++ src/App.js
@@ -45,9 +45,15 @@ const tempWatchedData = [
 const average = (arr) =>
     arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = "29fa2fc6";
+
 export default function App() {
-    const [movies, setMovies] = useState(tempMovieData);
-    const [watched, setWatched] = useState(tempWatchedData);
+    const [movies, setMovies] = useState([]);
+    const [watched, setWatched] = useState([]);
+
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=Interstellar`)
+        .then((res) => res.json())
+        .then((data) => setMovies(data.Search));
 
     return (
         <>
