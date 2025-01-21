c544fd4e542c81a85e6c4932fcf80e92e68e969a
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-21 17:22:51 +0800

    implement function to select and update movie detail when detail of another movie is shown

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ export default function App() {
     const [selectedId, setSelectedId] = useState(null);
 
     function handleSelectMovie(id) {
-        setSelectedId(id);
+        setSelectedId((selectedId) => (id === selectedId ? null : id));
     }
 
     function handleCloseMovie() {
