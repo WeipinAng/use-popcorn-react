a283523041767a88d4a8928ceef869212b8cc0b0
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-21 18:36:27 +0800

    allow user to add a watched movie to the list

--- src/App.js
+++ src/App.js
@@ -64,6 +64,10 @@ export default function App() {
         setSelectedId(null);
     }
 
+    function handleAddWatched(movie) {
+        setWatched((watched) => [...watched, movie]);
+    }
+
     useEffect(
         function () {
             async function fetchMovies() {
@@ -126,6 +130,7 @@ export default function App() {
                         <MovieDetails
                             selectedId={selectedId}
                             onCloseMovie={handleCloseMovie}
+                            onAddWatched={handleAddWatched}
                         />
                     ) : (
                         <>
@@ -266,12 +271,13 @@ function Movie({ movie, onSelectMovie }) {
     );
 }
 
-function MovieDetails({ selectedId, onCloseMovie }) {
+function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
     const [movie, setMovie] = useState({});
     const [isLoading, setIsLoading] = useState(false);
 
     const {
         Title: title,
+        Year: year,
         Poster: poster,
         Runtime: runtime,
         imdbRating,
@@ -281,6 +287,19 @@ function MovieDetails({ selectedId, onCloseMovie }) {
         Director: director,
         Genre: genre,
     } = movie;
+
+    function handleAdd() {
+        const newWatchedMovie = {
+            imdbID: selectedId,
+            title,
+            year,
+            poster,
+            imdbRating: Number(imdbRating),
+            runtime: Number(runtime.split(" ").at(0)),
+        };
+
+        onAddWatched(newWatchedMovie);
+    }
 
     useEffect(
         function () {
@@ -325,6 +344,9 @@ function MovieDetails({ selectedId, onCloseMovie }) {
                     <section>
                         <div className="rating">
                             <StarRating maxRating={10} size={24} />
+                            <button className="btn-add" onClick={handleAdd}>
+                                + Add to list
+                            </button>
                         </div>
                         <p>
                             <em>{plot}</em>
@@ -381,8 +403,8 @@ function WatchedMoviesList({ watched }) {
 function WatchedMovie({ movie }) {
     return (
         <li>
-            <img src={movie.Poster} alt={`${movie.Title} poster`} />
-            <h3>{movie.Title}</h3>
+            <img src={movie.poster} alt={`${movie.title} poster`} />
+            <h3>{movie.title}</h3>
             <div>
                 <p>
                     <span>⭐️</span>
