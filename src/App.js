edc58e2a08786b6bb5258d92e372721439b6122e
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-21 11:08:03 +0800

    convert effect to an async function instead of basic promise handling

--- src/App.js
+++ src/App.js
@@ -50,11 +50,17 @@ const KEY = "29fa2fc6";
 export default function App() {
     const [movies, setMovies] = useState([]);
     const [watched, setWatched] = useState([]);
+    const query = "Interstellar";
 
     useEffect(function () {
-        fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=Interstellar`)
-            .then((res) => res.json())
-            .then((data) => setMovies(data.Search));
+        async function fetchMovies() {
+            const res = await fetch(
+                `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            );
+            const data = await res.json();
+            setMovies(data.Search);
+        }
+        fetchMovies();
     }, []);
 
     return (
