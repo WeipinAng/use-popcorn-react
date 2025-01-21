ff4b301c95867a7d0635867a66f723a126f67e8d
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-21 19:00:04 +0800

    disallow user to add same or repeated movie to the watched list (display rating given before and cannot rate it anymore)

--- src/App.js
+++ src/App.js
@@ -131,6 +131,7 @@ export default function App() {
                             selectedId={selectedId}
                             onCloseMovie={handleCloseMovie}
                             onAddWatched={handleAddWatched}
+                            watched={watched}
                         />
                     ) : (
                         <>
@@ -271,10 +272,15 @@ function Movie({ movie, onSelectMovie }) {
     );
 }
 
-function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
+function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     const [movie, setMovie] = useState({});
     const [isLoading, setIsLoading] = useState(false);
     const [userRating, setUserRating] = useState("");
+
+    const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
+    const watchedUserRating = watched.find(
+        (movie) => movie.imdbID === selectedId
+    )?.userRating;
 
     const {
         Title: title,
@@ -346,15 +352,27 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
 
                     <section>
                         <div className="rating">
-                            <StarRating
-                                maxRating={10}
-                                size={24}
-                                onSetRating={setUserRating}
-                            />
-                            {userRating > 0 && (
-                                <button className="btn-add" onClick={handleAdd}>
-                                    + Add to list
-                                </button>
+                            {!isWatched ? (
+                                <>
+                                    <StarRating
+                                        maxRating={10}
+                                        size={24}
+                                        onSetRating={setUserRating}
+                                    />
+                                    {userRating > 0 && (
+                                        <button
+                                            className="btn-add"
+                                            onClick={handleAdd}
+                                        >
+                                            + Add to list
+                                        </button>
+                                    )}
+                                </>
+                            ) : (
+                                <p>
+                                    You rated this movie {watchedUserRating}{" "}
+                                    <span>⭐</span>
+                                </p>
                             )}
                         </div>
                         <p>
