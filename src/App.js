f60da7c4e21de636ac98fa395546c5355100ec53
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-21 11:33:43 +0800

    handle errors dealing with asynchronous data in web app (users suddenly lose internet connection)

--- src/App.js
+++ src/App.js
@@ -51,17 +51,29 @@ export default function App() {
     const [movies, setMovies] = useState([]);
     const [watched, setWatched] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
     const query = "Interstellar";
 
     useEffect(function () {
         async function fetchMovies() {
-            setIsLoading(true);
-            const res = await fetch(
-                `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
-            );
-            const data = await res.json();
-            setMovies(data.Search);
-            setIsLoading(false);
+            try {
+                setIsLoading(true);
+                const res = await fetch(
+                    `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+                );
+
+                if (!res.ok)
+                    throw new Error(
+                        "Something went wrong with fetching movies"
+                    );
+
+                const data = await res.json();
+                setMovies(data.Search);
+                setIsLoading(false);
+            } catch (err) {
+                console.error(err.message);
+                setError(err.message);
+            }
         }
         fetchMovies();
     }, []);
@@ -74,7 +86,10 @@ export default function App() {
             </NavBar>
             <Main>
                 <Box>
-                    {isLoading ? <Loader /> : <MovieList movies={movies} />}
+                    {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
+                    {isLoading && <Loader />}
+                    {!isLoading && !error && <MovieList movies={movies} />}
+                    {error && <ErrorMessage message={error} />}
                 </Box>
 
                 <Box>
@@ -98,6 +113,14 @@ export default function App() {
 
 function Loader() {
     return <p className="loader">Loading...</p>;
+}
+
+function ErrorMessage({ message }) {
+    return (
+        <p className="error">
+            <span>⛔</span> {message}
+        </p>
+    );
 }
 
 function NavBar({ children }) {
