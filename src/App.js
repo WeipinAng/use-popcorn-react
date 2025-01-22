f1e1926a1eb50214d73caad23ea3f4ab23d0277b
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-22 12:19:53 +0800

    fix bug of not focusing search element on initial render of app component after applying the use of Enter key

--- src/App.js
+++ src/App.js
@@ -231,6 +231,7 @@ function Search({ query, setQuery }) {
                 }
             }
             document.addEventListener("keydown", callback);
+            inputEl.current.focus();
 
             return () => document.removeEventListener("keydown", callback);
         },
